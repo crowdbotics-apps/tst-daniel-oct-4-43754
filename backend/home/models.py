618e375f350437a5554c91ac809bf6c4454508fc
crowdbotics-apps <team@crowdbotics.com>
2023-10-05 14:49:37 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Car(models.Model):
+    'Generated Model'
+    brand = models.CharField(max_length=255,)
+    model = models.CharField(max_length=255,)
+    date = models.DateField()
